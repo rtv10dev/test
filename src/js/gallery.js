23c8e1011b5e063682c61195a5b37a098495b3ff
SilviatovarDev <tovar.herrera.silvia@gmail.com>
2025-08-30 20:21:52 +0200

    solucion errores

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -4,7 +4,7 @@ export function cargarGaleria() {
   const grid = document.getElementById("galeria-json");
   if (!grid) return;
 
-  fetch("/maquetacionSTH/data/gallery.json") 
+  fetch(import.meta.env.BASE_URL + "gallery.json") 
     .then(res => res.json())
     .then(data => {
       grid.innerHTML = data
